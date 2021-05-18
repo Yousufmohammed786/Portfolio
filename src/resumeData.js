5bd14272a09b7454a3b8dfc462e747b32368cba5
Yousuf <mdyousuf.flex@gmail.com>
2021-05-18 15:30:37 +0530

    Second commit

---
+++ src/resumeData.js
@@ -0,0 +1,122 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Mohammed Yousuf",
+    "role": "Tech Lead and Frontend Developer",
+    "linkedinId":"Yousuf",
+    "skypeid": "Your skypeid",
+    "roleDescription": "To architect, design and develop applications and services manage product development cycle, collaborate with project teams and clients, and utilize technology to enhance business processes and deliver value-added products and services to internal and external customers.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Over 9 years of experience in UI/Web Development, designing User Interface (UI) applications and professional web applications using HTML5, DOM, CSS3, JavaScript, Adobe Flex, JQuery, Action Script, Angular JS with experience in the software development life cycle.	Worked in Agile Scrum Methodology - attended daily standups, Sprint review, retrospective and planning meetings.     	Hands on experience in UI development process such as Analysis, Development and Testing, and associated methodologies.     	Worked on Mobile-First Responsive Design using Bootstrap3, used Bootstrap templates for web applications. Good understanding of Grid Layout in CSS frameworks.   ",
+    "address":"India",
+    "website":"https://yousufmohammed786.github.io/portfolio/",
+    "education":[
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University Hyderabad",
+        "specialization":"B.Tech",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2009",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Jawaharlal Nehru Technological University Hyderabad",
+        "specialization":"M.Tech",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2012",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Tech Mahindra",
+        "specialization":"Tech Lead",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"May 2015 to still",
+        "Achievements":"Standing ovation Award of 2021"
+      },
+      {
+        "CompanyName":"Vcentric",
+        "specialization":"Senior Software engineer",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2015",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+     "styles" : [
+       {
+        "width":"90%"
+       },
+       {
+        "width":"80%"
+       },
+       {
+        "width":"70%"
+       }
+
+     ],
+      
+    "skills":[
+      {
+        "skillname":"Adobe Flex",
+        "Rating":"90%"
+      },
+      {
+        "skillname":"HTML, CSS",
+        "Rating":"80%"
+      },
+      {
+        "skillname":"Reactjs",
+        "Rating":"70%"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
